f173bfbf02a1599cb01d768462a8369e40baab8d
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 08:23:50 +1000

    typo

--- lib/ui.lua
+++ lib/ui.lua
@@ -143,7 +143,7 @@ else
     metro_icon(42,1, pos)
     screen.move(66, 6)
     --screen.text_right(data[data.pattern].bpm)
-    screen.text_right(math.floor(clock.get_tempo())
+    screen.text_right(math.floor(clock.get_tempo()))
     
     
   end
