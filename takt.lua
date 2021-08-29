5342bc9adf0c6f7b0d1720b30aa37ed9289ffc2b
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 18:18:24 +1000

    set substep loop to 16

--- takt.lua
+++ takt.lua
@@ -982,7 +982,8 @@ end
 
 function simple_seq()
   --clock.sync(1)
-  while true do
+  --while true do
+  for i=1,16 do
     simple_seqrun(math.floor(clock.get_beats()))
     clock.sync(1/64)
   end
