53024971ffa87bdbb8f975defa928695f0e93697
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 19:14:50 +1000

    128 steps, 1/64 sync

--- takt.lua
+++ takt.lua
@@ -983,10 +983,10 @@ end
 function simple_seq()
   --clock.sync(1)
   --while true do
-  for i=1,32 do
+  for i=1,256 do
     simple_seqrun(math.floor(clock.get_beats()))
-    --clock.sync(1/64)
   end
+  --clock.sync(1/64)
 end
 
 function test_seq()
