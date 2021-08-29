3cd207957f0d3b394343580f4ec0f4dac53d7d4d
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 11:12:52 +1000

    adding reset position every 4 bars

--- takt.lua
+++ takt.lua
@@ -962,16 +962,20 @@ end
 function sequencer()
     -- run the sequencer at 1/8 of a beat (ie. 1/32nd notes ) resolution 
     while is_running do
-        clock.sync(1/4)
-        --print(clock.get_tempo())
-        for i=1,16 do
-            seqrun(i) 
-            --if i % m_div(data.metaseq.div) == 0 then 
-            --    metaseq() 
-            --end 
-            --clock.sync(1/4) -- allow for 16 subdivisions per step
-            --stage = (stage + 1) 
-         end
+        clock.sync(4) -- stay in sync with link quantum
+        for j=1,64 do
+            clock.sync(1/4)
+            --print(clock.get_tempo())
+            for i=1,16 do
+                seqrun(i) 
+                --if i % m_div(data.metaseq.div) == 0 then 
+                --    metaseq() 
+                --end 
+                --clock.sync(1/4) -- allow for 16 subdivisions per step
+                --stage = (stage + 1) 
+             end
+             reset_positions()
+        end
     end
 end
 
