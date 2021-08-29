9552cf97ce1b72c99a53c0a80d0df018b1da5777
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 10:52:44 +1000

    256 steps sync 1/16

--- takt.lua
+++ takt.lua
@@ -964,11 +964,11 @@ function sequencer()
     while is_running do
         clock.sync(4)
         --print(clock.get_tempo())
-        for i=1,128 do
+        for i=1,256 do
             seqrun(i) 
-            if i % m_div(data.metaseq.div) == 0 then 
-                metaseq() 
-            end 
+            --if i % m_div(data.metaseq.div) == 0 then 
+            --    metaseq() 
+            --end 
             clock.sync(1/16) -- allow for 16 subdivisions per step
             --stage = (stage + 1) 
          end
