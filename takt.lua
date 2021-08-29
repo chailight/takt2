0776f44185d01ade85858e5da4612d856c2072ea
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 19:11:26 +1000

    32 substeps

--- takt.lua
+++ takt.lua
@@ -983,10 +983,10 @@ end
 function simple_seq()
   --clock.sync(1)
   --while true do
-  --for i=1,16 do
+  for i=1,32 do
     simple_seqrun(math.floor(clock.get_beats()))
     --clock.sync(1/64)
-  --end
+  end
 end
 
 function test_seq()
