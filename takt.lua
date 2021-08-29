407916af864ef692effe4876a6aac7a3a7dd5e00
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 13:38:28 +1000

    1/4 16x16 1/64

--- takt.lua
+++ takt.lua
@@ -962,7 +962,7 @@ end
 function sequencer()
     -- run the sequencer at 1/8 of a beat (ie. 1/32nd notes ) resolution 
     while is_running do
-        clock.sync(1) -- stay in sync with link quantum
+        clock.sync(1/4) -- stay in sync with link quantum
         for j=1,16 do -- 
             --clock.sync(1/4)
             --print(clock.get_tempo())
