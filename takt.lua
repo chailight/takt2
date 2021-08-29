a7a2de9fa78547d82024e850a82fd0f7b88c6415
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 22:54:18 +1000

    sync 1/4 and 1/64

--- takt.lua
+++ takt.lua
@@ -985,6 +985,7 @@ function simple_seq()
   while true do
       for i=1,16 do
         simple_seqrun(math.floor(clock.get_beats()))
+        clock.sync(1/64)
       end
       clock.sync(1/4)
   end
