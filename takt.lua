85b7abe501610d73b8ef7f06347ec65fc68e2efd
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 13:35:57 +1000

    64 x 16 / 1/96

--- takt.lua
+++ takt.lua
@@ -971,7 +971,7 @@ function sequencer()
                 --if i % m_div(data.metaseq.div) == 0 then 
                 --    metaseq() 
                 --end 
-                clock.sync(1/128) -- allow for 16 subdivisions per step
+                clock.sync(1/96) -- allow for 16 subdivisions per step
                 --stage = (stage + 1) 
              end
         end
