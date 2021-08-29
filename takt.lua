53781862b3e0a0110caa1ef1545dfdf9175e75af
Mark Pedersen <markp@192-168-1-108.tpgi.com.au>
2021-08-29 19:48:04 +1000

    typo

--- takt.lua
+++ takt.lua
@@ -983,7 +983,7 @@ end
 function simple_seq()
   --clock.sync(1)
   --while true do
-  for i=1,2 do
+  for i=1,1 do
     simple_seqrun(math.floor(clock.get_beats()))
     --clock.sync(1/64)
   end
@@ -1011,7 +1011,7 @@ local function simple_advance_step(tr, counter)
 end
 
 function simple_seqrun(counter)
-  for tr = 1, 1 do
+  for tr = 1, 2 do
 
       local div = data[data.pattern].track.div[tr]
       
